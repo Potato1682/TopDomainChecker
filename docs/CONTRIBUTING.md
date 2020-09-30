8eceef98b5824dd983b9960d896b85871e79d7be
Potato1682 <potato1682.imo@gmail.com>
2020-09-30 23:06:05 +0900

    QoL改善 ・CodacyのIssue完全制覇 ・Remarkの導入 ・ESLintルールの見直し ・npm-scriptsの追加 ・package.jsonの移動 ・ESLintパーサーの変更 ・.eslintrcのYAML化

--- docs/CONTRIBUTING.md
+++ docs/CONTRIBUTING.md
@@ -8,19 +8,19 @@ this repository has established some rules to properly contribute as TopDomainCh
 
 ### Issue, Pull Request manners
 
-- Try to minimize duplicate issue.  
-  The duplicate issue has a \[duplicate\] label and a reference to the issue, so if you notice it, close it.
-- When asking questions in Issue, keep in mind **smart questions**.  
-  Please use the \[question\] label when asking a question.
-- Try to use pull requests and publishing templates as much as possible.  
+-   Try to minimize duplicate issue.  
+    The duplicate issue has a \[duplicate\] label and a reference to the issue, so if you notice it, close it.
+-   When asking questions in Issue, keep in mind **smart questions**.  
+    Please use the \[question\] label when asking a question.
+-   Try to use pull requests and publishing templates as much as possible.  
 
 ### Commit manners
 
-- If you have a GPG key, feel free to sign it.
-- If you check syntax, then commit.  
-  If you are not sure, commit the fixed changes.
-  > **Note: Do not make significant changes in the fix commit.**
+-   If you have a GPG key, feel free to sign it.
+-   If you check syntax, then commit.  
+    If you are not sure, commit the fixed changes.
+    > **Note: Do not make significant changes in the fix commit.**
 
 ### See also
 
-[Code of Conduct](CODE_OF_CONDUCT.md)
+-   [Code of Conduct](CODE_OF_CONDUCT.md)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import fs from "fs"
+import https from "https"
+import readline from "readline"
 import boxen from "boxen"
 import chalk from "chalk"
 import cliCursor from "cli-cursor"
@@ -5,12 +8,9 @@ import ncp from "copy-paste"
 import commandLineArgs from "command-line-args"
 import commandLineUsage from "command-line-usage"
 import figures from "figures"
-import fs from "fs"
-import https from "https"
 import inquirer from "inquirer"
 import ping from "ping"
 import prettyerror from "pretty-error"
-import readline from "readline"
 import terminalLink from "terminal-link"
 
 // Call console beautify functions
@@ -18,16 +18,18 @@ prettyerror.start()
 cliCursor.hide()
 
 // Command usages and arguments
-const usage = commandLineUsage([{
-    header: "TopDomainChecker",
-    content: "Brute-force the top-level domain with {italic parallel}."
-},
+const usage = commandLineUsage([
+    {
+        header: "TopDomainChecker",
+        content: "Brute-force the top-level domain with {italic parallel}."
+    },
     {
         header: "Main Values",
-        content: [{
-            name: "{bold [stdin]}",
-            summary: "Get the domain to search from the standard input. \n If the standard input does not find anything, you will be prompted with \n an argument or interactively."
-        },
+        content: [
+            {
+                name: "{bold [stdin]}",
+                summary: "Get the domain to search from the standard input. \n If the standard input does not find anything, you will be prompted with \n an argument or interactively."
+            },
             {
                 name: "{bold [domain]}",
                 summary: "{underline TopDomainChecker prioritizes arguments over standard inputs}. \n If it missing, show the interactive prompt."
@@ -75,26 +77,19 @@ const usage = commandLineUsage([{
             {
                 name: "add-tld",
                 alias: "t",
-                description: "Enter additional top-level domains separated by spaces. If you use only the flag, show interactive prompt.",
+                description: "Enter additional top-level domains separated by spaces. If you use only the flag, show interactive prompt."
             }
         ]
     },
     {
         content: `Project home: ${terminalLink(
             "GitHub",
-            "https://github.com/P2P-Develop/TopDomainChecker")}`
+            "https://github.com/P2P-Develop/TopDomainChecker"
+        )}`
     }
 ])
 
-const args = commandLineArgs([
-    {name: "version", alias: "V", type: Boolean, defaultValue: false},
-    {name: "verbose", alias: "v", type: Boolean, defaultValue: false},
-    {name: "domain", type: String, multiple: true, defaultOption: true},
-    {name: "help", alias: "h", type: Boolean, defaultOption: false},
-    {name: "quiet", alias: "q", type: Boolean, defaultValue: false},
-    {name: "no-box", alias: "Q", type: Boolean, defaultValue: false},
-    {name: "add-tld", alias: "t"}
-])
+const args = commandLineArgs([{name: "version", alias: "V", type: Boolean, defaultValue: false}, {name: "verbose", alias: "v", type: Boolean, defaultValue: false}, {name: "domain", type: String, multiple: true, defaultOption: true}, {name: "help", alias: "h", type: Boolean, defaultOption: false}, {name: "quiet", alias: "q", type: Boolean, defaultValue: false}, {name: "no-box", alias: "Q", type: Boolean, defaultValue: false}, {name: "add-tld", alias: "t"}])
 
 if (args.version) {
     console.log("v2.0.0")
@@ -126,8 +121,9 @@ const stdin = (() => {
     }
 })()
 
-if (stdin !== undefined)
-    args.domain = [...args.domain, ...stdin.trim().split(" ")] // If stdin has an input, merge domains
+if (stdin !== undefined) 
+    args.domain = [ ...args.domain, ...stdin.trim().split(" ") ]
+// If stdin has an input, merge domains
 
 // If also not, show cursor and interactive prompt
 if (!("domain" in args)) {
@@ -162,40 +158,26 @@ const gets = domains => {
                 if (res.alive) {
                     aliveDomain.push(domain)
                     if (!args.quiet) {
-                        process.stdout.write(
-                            `\n${chalk.greenBright.inverse(
-                                `  ${figures.tick}  `
-                            )}  ${chalk.bold.cyan(domain)} is ${chalk.greenBright(
-                                "up"
-                            )}` +
-                            " ".repeat(process.stdout.columns - `\n${chalk.greenBright.inverse(
-                                `  ${figures.tick}  `
-                            )}  ${chalk.bold.cyan(domain)} is ${chalk.greenBright(
-                                "up"
-                            )}`.length)
-                        )
-                        if (!args.verbose) readline.moveCursor(process.stdout, 0, -1)
-                        else console.log()
+                        process.stdout.write(`\n${chalk.greenBright.inverse(`  ${figures.tick}  `)}  ${chalk.bold.cyan(domain)} is ${chalk.greenBright("up")}` +
+                            " ".repeat(process.stdout.columns - `\n${chalk.greenBright.inverse(`  ${figures.tick}  `)}  ${chalk.bold.cyan(domain)} is ${chalk.greenBright("up")}`.length))
+                        if (!args.verbose) 
+                            readline.moveCursor(process.stdout, 0, -1)
+                        else 
+                            console.log()
+                        
                     }
                     return Promise.resolve()
                 }
 
                 if (!args.quiet) {
-                    process.stdout.write(
-                        `\n${chalk.redBright.inverse(
-                            `  ${figures.cross}  `
-                        )}  ${chalk.bold.cyan(domain)} is ${chalk.redBright(
-                            "down"
-                        )}` +
-                        " ".repeat(process.stdout.columns - `\n${chalk.redBright.inverse(
-                            `  ${figures.cross}  `
-                        )}  ${chalk.bold.cyan(domain)} is ${chalk.redBright(
-                            "down"
-                        )}`.length)
-                    )
+                    process.stdout.write(`\n${chalk.redBright.inverse(`  ${figures.cross}  `)}  ${chalk.bold.cyan(domain)} is ${chalk.redBright("down")}` +
+                        " ".repeat(process.stdout.columns - `\n${chalk.redBright.inverse(`  ${figures.cross}  `)}  ${chalk.bold.cyan(domain)} is ${chalk.redBright("down")}`.length))
 
-                    if (!args.verbose) readline.moveCursor(process.stdout, 0, -1)
-                    else console.log()
+                    if (!args.verbose) 
+                        readline.moveCursor(process.stdout, 0, -1)
+                    else 
+                        console.log()
+                    
                 }
                 return Promise.resolve()
             })
@@ -231,19 +213,23 @@ const main = (tlds) => {
             }))
 
         console.log("\n\n")
-    } else args.domain.forEach(d => (tld => tlds.map(`${d}.${tld}`)).forEach(uri => order.push(uri)))
+    } else 
+        args.domain.forEach(d => (tld => tlds.map(`${d}.${tld}`)).forEach(uri => order.push(uri)))
+    
 
     gets(order)
 
     setTimeout(() => {
-        if (!args["no-box"])
+        if (!args["no-box"]) 
             console.log(boxen(`${chalk.bold.underline("--- Result---")}\n\n${chalk.bold.blueBright(aliveDomain.join("\n"))}\n\n${chalk.bold.cyanBright(aliveDomain.length)} ${chalk.greenBright(aliveDomain.length > 1 ? "domains alive" : "domain alive")}`, {
                 padding: 1,
                 borderColor: "yellow",
                 margin: 2,
                 align: "center"
             }))
-        else console.log(aliveDomain.join("\n"))
+        else 
+            console.log(aliveDomain.join("\n"))
+        
     }, 10000)
 }
 
@@ -253,14 +239,17 @@ if (args.verbose) console.log(`\n${chalk.greenBright(figures.pointer)} Fetching 
 
 https.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt", (res) => {
     res.on("data", (chunk) => {
-        if (!lockFlag) {
-            main(
-                [...(`${chunk}`
+        if (!lockFlag) 
+            main([
+                ...(`${chunk}`
                     .toLowerCase()
                     .split(/\r\n|\n/)
-                    .filter(tld => !tld.startsWith("#") || !tld)), ...addTld]
-            )
-        } else lockFlag = true
+                    .filter(tld => !tld.startsWith("#") || !tld)),
+                ...addTld 
+            ])
+        else 
+            lockFlag = true
+        
     })
 }).on("error", e => {
     console.error(e)
@@ -269,5 +258,6 @@ https.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt", (res) => {
         type: "confirm",
         name: "copy",
         message: "Copy stack-trace to clip board?"
-    }).then(ans => ans)) ncp.copy(e)
+    }).then(ans => ans)) 
+        ncp.copy(e)
 })
